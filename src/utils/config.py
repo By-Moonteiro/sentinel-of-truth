ee467d7edcd6b5824da835a67eb1082cd98563fa
Moonteiro <owagnerlucas0@gmail.com>
2025-10-31 09:37:14 -0300

    feat: adiciona dicionário de status

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,4 +1,12 @@
 from pathlib import Path
 
+# Caminhos para os arquivos
 DADOS = Path(__file__).parent.parent.parent / "data" / "saved_news.json"
 DADOS_BACKUP = Path(__file__).parent.parent.parent / "data"
+
+# Status
+STATUS = {
+    "1": "Verdadeiro",
+    "2": "Falso",
+    "3": "Não Checado"
+}
