4e2a3fa6eeb78567e085e815a4be4a7a68d4a5c0
Moonteiro <owagnerlucas0@gmail.com>
2025-10-31 20:43:50 -0300

    refactor: padroniza estrutura de dados das noticias

--- src/logic/manager.py
+++ src/logic/manager.py
@@ -115,27 +115,16 @@ class ManageNews:
             list: Lista de Noticias com o status especifico
         """
         loaded_news = Handler.load_date()
-        filter_status = []
+        filter_status = {}
 
         for id_news, news in loaded_news.items():
             if news[1] == status:
-                filter_status.append({"id": id_news, "url": news[0], "status": news[1]})
+                filter_status[id_news] = {
+                    "url": news[0],
+                    "status": news[1]
+                }
 
         return filter_status
-
-
-    def display_news_status(self, noticias: list) -> None:
-        """
-        Exibe cada notícia formatada.
-
-        Args:
-            noticias(list): Lista de notícias para exibir
-
-        Returns:
-            None: Função apenas exibe output na tela
-        """
-        for noticia in noticias:
-            print(f"ID: {noticia["id"]} | URL: {noticia["url"]} | Status: {noticia["status"]}\n")
 
 
     def display_news(self, noticias: dict) -> None:
