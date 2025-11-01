894ebf59dca58c1dc3f505e2ecf95fdddabfa622
Moonteiro <owagnerlucas0@gmail.com>
2025-11-01 17:25:58 -0300

    feat: adiciona a importação de limpar o terminal no init

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,5 @@
 from .validation import menu_validation, valid_status
 from .json_handler import Handler
-from .config import DATA, DATA_BACKUP, STATUS, REPORT
+from .config import clear_screen, DATA_BACKUP, STATUS, REPORT, DATA
 
-__all__ = [menu_validation, valid_status, Handler, DATA, DATA_BACKUP, STATUS, REPORT]
+__all__ = [menu_validation, valid_status, Handler, clear_screen, DATA, DATA_BACKUP, STATUS, REPORT]
