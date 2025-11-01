76c539e6f276883e6dc9adde0e6dd7b9c5075bd7
Moonteiro <owagnerlucas0@gmail.com>
2025-11-01 14:43:53 -0300

    feat: adiciona caminho para o relatório

--- src/utils/config.py
+++ src/utils/config.py
@@ -4,6 +4,7 @@ from pathlib import Path
 # Caminhos para os arquivos
 DADOS = Path(__file__).parent.parent.parent / "data" / "saved_news.json"
 DADOS_BACKUP = Path(__file__).parent.parent.parent / "data"
+RELATORIO = Path(__file__).parent.parent.parent
 
 # Status
 STATUS = {"1": "Verdadeiro", "2": "Falso", "3": "Não Checado"}
