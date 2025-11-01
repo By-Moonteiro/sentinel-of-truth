abfc2917d1d9790d9381c4beb84aa961e1bee544
Moonteiro <owagnerlucas0@gmail.com>
2025-11-01 15:05:15 -0300

    feat: adiciona import para o caminho do relatório

--- src/logic/__init__.py
+++ src/logic/__init__.py
@@ -1,4 +1,5 @@
 from .news import News
 from .manager import id_generation, ManageNews
+from .report import ReportNews
 
-__all__ = [News, id_generation, ManageNews]
+__all__ = [News, id_generation, ManageNews, ReportNews]
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,5 @@
 from .validation import menu_validation, valid_status
 from .json_handler import Handler
-from .config import DADOS, DADOS_BACKUP, STATUS, REPORT
+from .config import DATA, DATA_BACKUP, STATUS, REPORT
 
-__all__ = [menu_validation, valid_status, Handler, DADOS, DADOS_BACKUP, STATUS, REPORT]
+__all__ = [menu_validation, valid_status, Handler, DATA, DATA_BACKUP, STATUS, REPORT]
