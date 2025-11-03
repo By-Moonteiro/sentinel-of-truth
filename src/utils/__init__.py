cd2e99323d7f6893bdf6071e27e483f39418bdfa
Moonteiro <owagnerlucas0@gmail.com>
2025-11-03 19:57:43 -0300

    chore: adiciona as funções do arquivo menu ao __all__

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,6 +1,7 @@
 from .validation import menu_validation, valid_status
 from .json_handler import Handler
 from .config import clear_screen, DATA_BACKUP, STATUS, REPORT, DATA
+from .menu import display_main_menu, display_sub_menu
 
 __all__ = [
     "menu_validation",
@@ -11,4 +12,6 @@ __all__ = [
     "DATA_BACKUP",
     "STATUS",
     "REPORT",
+    "display_main_menu",
+    "display_sub_menu"
 ]
