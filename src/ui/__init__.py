8ec6764918c08785ec5742b9e83babb3734fe483
Moonteiro <owagnerlucas0@gmail.com>
2025-11-08 18:57:02 -0300

    refactor(__init__): adiciona importação de classe e implementa no __all__

--- src/ui/__init__.py
+++ src/ui/__init__.py
@@ -1,14 +1,8 @@
-from .display import (
-    display_all_news, 
-    display_news, 
-    display_news_by_status
-    )
+from .display import Display
 from .menu import display_main_menu, display_sub_menu
 
 __all__ = [
-    "display_all_news", 
-    "display_news", 
-    "display_news_by_status", 
+    "Display",
     "display_main_menu", 
     "display_sub_menu"
     ]
