d9517d3a10f1ba7037323855343b814c8ec6edb2
Moonteiro <owagnerlucas0@gmail.com>
2025-11-08 18:36:42 -0300

    refactor(display): refatora método para exibir as notícias por status

--- src/ui/display.py
+++ src/ui/display.py
@@ -25,7 +25,7 @@ class Display:
         print("=" * 60)
 
 
-    def display_news_by_status(manager, title: str, status: str) -> None:
+    def display_news_by_status(self, title: str, status: str) -> None:
         """
         Exibe as notícias filtradas por status
         Função genérica para qualquer status
@@ -38,29 +38,16 @@ class Display:
         print("=" * 60)
         print("{:^60}".format(f"{title}\n"))  # <- Cabeçalho
         print("=" * 60)
-        news = manager.search_status_news(f"{status}")
-        manager.display_news(news)  # <- Exibe as notícias por status
+        status_news = self.manager.search_status_news(status)
+
+        if not status_news:
+             print("Nenhuma notícia cadastrada")
+        else:
+
+            for news_id, url, status_value in status_news:
+                print(f"Id: {news_id} | Url: {url} | Status: {status_value}")
+                
         print("=" * 60)
-
-
-
-    def display_news(self, noticias: dict) -> None:
-            """
-            Exibe cada notícia formatada.
-
-            Args:
-                noticias(dict): Dicionário de notícias para exibir
-
-            Returns:
-                None: Função apenas exibe output na tela
-            """
-
-            if not noticias:  # <- Caso não tenha notícias
-                print("Não tem noticias para ser apresentada")
-                return
-
-            for id_news, news in noticias.items():
-                print(f"ID: {id_news} | URL: {news[0]} | Status: {news[1]}\n")
 
 
     def wait_for_enter() -> None:
