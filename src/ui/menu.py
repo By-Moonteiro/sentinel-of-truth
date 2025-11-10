413101ab3c089b3762200d70e26e65ab5cf11552
Moonteiro <owagnerlucas0@gmail.com>
2025-11-10 12:41:50 -0300

    feat(menu): implementa função de exibição das opções do sub-menu

--- src/ui/menu.py
+++ src/ui/menu.py
@@ -1,4 +1,7 @@
-def display_main_menu() -> None:
+from .display import Display
+from src.utils.helpers import clear_screen
+
+def main_menu() -> None:
     """Exibe o menu principal"""
     print("\n╔══════════════════════════════╗")
     print("║      SENTINEL OF TRUTH       ║")
@@ -12,7 +15,7 @@ def display_main_menu() -> None:
     print("╚══════════════════════════════╝")
 
 
-def display_sub_menu() -> None:
+def sub_menu() -> None:
     """Exibe o sub-menu"""
     print("\n╔═════════════════════════════════╗")
     print("║            CHECK NEWS           ║")
@@ -23,3 +26,42 @@ def display_sub_menu() -> None:
     print("║ 4 - Ver notícias NÃO CHECADAS   ║")
     print("║ 5 - Retornar                    ║")
     print("╚═════════════════════════════════╝")
+
+def sub_menu_options(sub_option: int) -> None:
+    """
+    Exibe opções do sub-menu
+
+    Args:
+        sub_option(int): Opção inserida pelo usuário
+
+    Returns:
+        None: Executa a opção desejada
+    """
+    # Instância
+    display = Display()
+
+
+    if sub_option == 1:  # <- Exibe todas as notícias
+        display.display_all_news()
+        display.wait_for_enter()  # <- Aguarda o usuário pressionar Enter p/ continuar
+        clear_screen()
+
+    elif sub_option == 2:
+        display.display_news_by_status("NOTÍCIAS VERDADEIRAS", "Verdadeiro")
+        display.wait_for_enter()
+        clear_screen()
+
+    elif sub_option == 3:  # <- Exibe somente as notícias Falsas
+        display.display_news_by_status("NOTÍCIAS FALSAS", "Falso")
+        display.wait_for_enter()
+        clear_screen()
+
+    elif sub_option == 4:
+        display.display_news_by_status(
+                "NOTÍCIAS NÃO CHECADAS", "não_checado"
+                )
+        display.wait_for_enter()
+        clear_screen()
+
+    elif sub_option == 5:  # <- Retorna pro Menu Principal
+        clear_screen()    
